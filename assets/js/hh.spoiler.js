a5f2958e896cd1af0c69a42417b7d81dcf022acc
James Daly <da_j@live.co.uk>
2018-09-13 01:09:08 +0100

    Fix spoiler (children), small style changes

--- assets/js/hh.spoiler.js
+++ assets/js/hh.spoiler.js
@@ -5,16 +5,20 @@ document.addEventListener('DOMContentLoaded', function () {
     el.setAttribute('data-spoiler', ++spoilern)
 
     let spoilerLink = document.createElement('a')
-    spoilerLink.setAttribute('class', '.requisite-warning .warning .compact spoiler-link')
+    spoilerLink.setAttribute('class', 'requisite-warning warning compact spoiler-link')
     spoilerLink.setAttribute('data-spoil', spoilern.toString())
     spoilerLink.setAttribute('href', 'javascript:void(0)')
     spoilerLink.innerHTML = el.getAttribute('data-spoiler-text') || 'At DigiMakers events, this has already been done for you. <em>Click to show anyway</em>'
     spoilerLink.addEventListener('click', e => {
       // @ts-ignore
-      let target = document.querySelector('[data-spoiler="' + e.target.getAttribute('data-spoil') + '"]')
+      let link = e.target
+      // @ts-ignore
+      while (!link.hasAttribute('data-spoil')) link = link.parentNode
+      // @ts-ignore
+      let target = document.querySelector('[data-spoiler="' + link.getAttribute('data-spoil') + '"]')
       target.setAttribute('class', target.getAttribute('class').replace(/spoiler-hidden/g, ''))
       // @ts-ignore
-      e.target.parentNode.removeChild(e.target)
+      link.parentNode.removeChild(link)
     })
 
     el.parentNode.insertBefore(spoilerLink, el)
